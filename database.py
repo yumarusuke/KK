67284661b6dbb13d4c67f0d624c0b82eec275759
yumarusuke <yubaseballfunya@gmail.com>
2025-09-26 18:08:11 +0900

    “new”

--- database.py
+++ database.py
@@ -6,6 +6,29 @@ class Person(Model):
     name = CharField()
     age = IntegerField()
     gender = CharField()
+    mailaddress = CharField()
+    password = IntegerField()
+    password2 = IntegerField()
+    allergie = CharField()
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+class Family(Model):
+    name = CharField()
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+class Shohin(Model):
+    name = CharField()
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+class store(Model):
+    name = CharField()
+    place = CharField()
 
     class Meta:
         database = db # This model uses the "people.db" database.
